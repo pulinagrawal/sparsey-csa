d9d11084c23713f9c68d7299aba484a420ed9b2d
Pulin Agrawal <pulinagrawal@gmail.com>
2023-02-27 16:27:41 -0500

    add resnet CNN test case

--- test/test_smcsa.py
+++ test/test_smcsa.py
@@ -1,21 +1,25 @@
-import unittest
-import numpy as np
-from tqdm import tqdm
-
 import sys
 from pathlib import Path
 sys.path.append(str(Path('.').absolute()))
 
-from src.structure.ncsa import SegmentedMacrocolumn
-from hypothesis import given, strategies as st
+import torchvision.transforms as transforms
+from PIL import Image
+import torchvision
 import torch
+from hypothesis import given, strategies as st
+from src.structure.ncsa import SegmentedMacrocolumn
+import unittest
+import numpy as np
+from tqdm import tqdm
 
-#np.random.seed(1111)
+# np.random.seed(1111)
 INPUT_SIZE = 512
+
 
 def random_input(size):
     v = torch.rand((size,))
     return v
+
 
 class TestSMCSA(unittest.TestCase):
 
@@ -25,9 +29,9 @@ class TestSMCSA(unittest.TestCase):
         input = random_input(512)
         output1 = m.run(input).flatten()
         output2 = m.run(input).flatten()
-        
+
         hamming = sum(abs(output2-output1))
-        assert hamming < sum(output1)*.2
+        assert hamming < len(output1)*.005
 
     def test_consecutive_diff_input(self):
         m = SegmentedMacrocolumn(2048, 512, .02)
@@ -48,7 +52,7 @@ class TestSMCSA(unittest.TestCase):
 
         input = random_input(512)
         output1 = m.run(input).flatten()
-        
+
         for i in range(10):
             input_others = random_input(512)
             _ = m.run(input_others).flatten()
@@ -64,7 +68,7 @@ class TestSMCSA(unittest.TestCase):
 
         input = random_input(512)
         output1 = m.run(input).flatten()
-        
+
         for i in tqdm(range(1000)):
             input_others = random_input(512)
             _ = m.run(input_others).flatten()
@@ -78,3 +82,31 @@ class TestSMCSA(unittest.TestCase):
 
     # TODO Create a test case that tests similarity of representations
     # for two inputs from outputs of ResNet50 from similar and different images
+
+    def test_resnet_output(self):
+        transform = transforms.Compose([
+            transforms.PILToTensor(),
+            transforms.Resize((400, 600)),
+        ])
+        images = []
+        for f in Path('data/db').iterdir():
+            with f.open('rb') as fp:
+                images.append(transform(Image.open(fp)))
+        images = torch.stack(images)
+        images = images/255.
+        device = 'cpu'
+        images.to(device)
+        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
+            pretrained=True, device=device).to(device)
+        with torch.no_grad():
+            features = model.backbone(images)
+        print(features)
+
+        feature_size = len(features['3'][0].flatten())
+        m = SegmentedMacrocolumn(2048, feature_size, .02)
+
+        output1 = m.run(features['3'][0].flatten()).flatten()
+        output2 = m.run((features['3'][1].flatten())).flatten()
+
+        hamming = sum(abs(output2-output1))
+        assert hamming < len(output1)*.02
